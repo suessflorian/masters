a775f8500796368fa3d67f82a75e8ed652e472cf
Florian Suess <floriansuess96@icloud.com>
2022-10-09 13:59:23 +1300

    Some comments describing the simplicity of `mapcolouring`

--- cs761/prolog/mapcolouring.pl
+++ cs761/prolog/mapcolouring.pl
@@ -1,3 +1,9 @@
+/*
+* given knowledge base, one thing to check would be that
+* different(C1, C2) :- different(C2, C1)
+* but that is already implied due to the exhaustive listing
+*/
+
 different(red, green).
 different(red, blue).
 different(green, blue).
@@ -5,14 +11,23 @@ different(green, red).
 different(blue, red).
 different(blue, green).
 
+/*
+* pretty straight forward - and due to the above comment,
+* we don't need to say that for example;
+* "WA should be different to A"
+* and "A should be different to WA", just one of those is
+* sufficient.
+*/
+
 mapcolouring(N, A, WA, B, G, T, M, H, WE) :-
-				different(N, A),
-				different(A, WA),
-				different(WA, T),
-				different(WA, H),
-				different(WA, B),
-				different(WA, M),
-				different(M, WE),
-				different(M, H),
-				different(B, H),
-				different(B, G).
+	different(N, A),
+	different(A, WA),
+	different(WA, T),
+	different(WA, M),
+	different(WA, H),
+	different(WA, B),
+	different(M, WE),
+	different(M, H),
+	different(H, B),
+	different(H, G),
+	different(B, G).
