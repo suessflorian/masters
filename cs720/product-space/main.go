65f94feb538a57b0a36abe54b0d7149c857c522f
Florian Suess <floriansuess96@icloud.com>
2023-03-13 14:41:05 +1300

    Generalise

--- cs720/product-space/main.go
+++ cs720/product-space/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println(enumerateGrayCodes(3))
+	// fmt.Println(enumerateGrayCodes(3))
+	fmt.Println(enumerateProductSpaces([]int{3, 2, 2}))
 }
 
 func enumerateGrayCodes(n int) []string {
@@ -12,13 +16,36 @@ func enumerateGrayCodes(n int) []string {
 	}
 
 	var list []string
-
 	var childrenCodes = enumerateGrayCodes(n - 1)
 	for _, code := range childrenCodes {
 		list = append(list, "0"+code)
 	}
 	for i := len(childrenCodes) - 1; i >= 0; i-- {
 		list = append(list, "1"+childrenCodes[i])
+	}
+
+	return list
+}
+
+func enumerateProductSpaces(spaces []int) []string {
+	if spaces == nil || len(spaces) == 0 {
+		return []string{""}
+	}
+
+	var list []string
+	var childrenEnumerations = enumerateProductSpaces(spaces[1:])
+	currentSpace := spaces[0]
+
+	for prefix := 0; prefix < currentSpace; prefix++ {
+		if prefix%2 == 0 {
+			for _, enumeration := range childrenEnumerations {
+				list = append(list, strconv.Itoa(prefix)+enumeration)
+			}
+		} else {
+			for i := len(childrenEnumerations) - 1; i >= 0; i-- {
+				list = append(list, strconv.Itoa(prefix)+childrenEnumerations[i])
+			}
+		}
 	}
 
 	return list
